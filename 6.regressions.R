cc250256c5cd699c009b181e0f9bff2ec54f0ed6
CHNYNG <652059698@qq.com>
2023-08-30 10:45:15 +0800

    8.30

--- 6.regressions.R
+++ 6.regressions.R
@@ -5,7 +5,7 @@
 #把所有的数据集合起来
 
 library(dplyr)
-=======
+
 ######
 #加载前步骤
 #####
@@ -56,74 +56,10 @@ reg <- reg %>%
 #####
 #weitao:做回归之前先检查下自相关
 #检查一下可能相关的变量的自相关关系
-library(GGally)
-reg_myco <- reg[,c("qr_AM", "qr_EM", "qr_BZ", "qr_Pn","qr_Pq" )]
-p_reg_myco <- ggpairs(reg_myco, title="correlogram of mycorrhiza") 
-#qr_AM与qr_BZ、qr_Pn不显著
-#qr_EM与qr_BZ、qr_Pn、qr_Pq均不显著
-#qr_Pn与qr_Pq不显著
-ggsave("pic/p_reg_myco.pdf", plot = p_reg_myco, width = 11.69, height = 8.27, units = "in", device = "pdf")
-
-#关于生长的变量需要再算一个growth_rate
-reg <- reg %>%
-  mutate(growth_rate= log(DBH2)/log(DBH1),
-  growth_rate = ifelse(is.infinite(growth_rate), 0,
-                       growth_rate))
-
-reg_gr <- reg[,c("DBH1", "DBH2", "growth_rate")]
-p_reg_gr<- ggpairs(reg_gr, title="correlogram of growth")
-#三者相关性显著
-ggsave("pic/p_reg_gr.pdf", plot = p_reg_gr, width = 11.69, height = 8.27, units = "in", device = "pdf")
-
-
-#关于根系性状的变量的相关
-reg_root <- reg[,c("AD", "SRL", "SRA")]
-p_reg_root <- ggpairs(reg_root, title="correlogram of root")
-#三者相关性显著
-ggsave("pic/p_reg_root.pdf", plot = p_reg_root, width = 11.69, height = 8.27, units = "in", device = "pdf")
-
-
-#关于土壤元素
-reg_soil <- reg[,c("soc", "tn", "tp", "ap", "ph")]
-p_reg_soil <- ggpairs(reg_soil, title="correlogram of soil")
-#其中ap和soc的相关性不显著
-ggsave("pic/p_reg_soil.pdf", plot = p_reg_soil, width = 11.69, height = 8.27, units = "in", device = "pdf")
-
-#关于谱系（我好懒，想把它们搞一起
-reg_phylo <- reg[,c("pd20_unweigh", "mpd20_unweigh", "mpd20_weigh", "mntd20_unweigh", "mntd20_weigh",
-                    "pd10_unweigh", "mpd10_unweigh", "mpd10_weigh", "mntd10_unweigh", "mntd10_weigh",
-                    "pd50_unweigh", "mpd50_unweigh", "mpd50_weigh", "mntd50_unweigh", "mntd50_weigh")]
-
-p_reg_phylo <- ggpairs(reg_phylo, title="correlogram of phylo")
-ggsave("pic/p_reg_phylo.pdf", plot = p_reg_phylo, width = 11.69, height = 8.27, units = "in", device = "pdf")
-
-#关于邻体多样性
-reg_div <- reg[,c("shannon_div_20", "invsimpson_div_20", "simpson_div_20",
-                  "shannon_div_10", "invsimpson_div_10", "simpson_div_10",
-                  "shannon_div_50", "invsimpson_div_50", "simpson_div_50",)]
-
-p_reg_div <- ggpairs(reg_div, title="correlogram of divsity")
-ggsave("pic/p_reg_div.pdf", plot = p_reg_div, width = 11.69, height = 8.27, units = "in", device = "pdf")
-
-#关于邻体影响力
-reg_nei <- reg[,c("BD_20", "CBD_20", "HBD_20",
-                  "BD_10", "CBD_10", "HBD_10",
-                  "BD_50", "CBD_50", "HBD_50")]
-
-p_reg_nei <- ggpairs(reg_nei, title="correlogram of neighbors")
-ggsave("pic/p_reg_nei.pdf", plot = p_reg_nei, width = 11.69, height = 8.27, units = "in", device = "pdf")
-
-correlation_matrix <- cor(reg[,14:37])
-# 查看相关性矩阵
-print(correlation_matrix)
-
-#按照侵染率、根系、土壤、植物、谱系参数、多样性指数
-#分别计算变量间的相关性
-Scatterplot <- ggpairs(reg[,14:37], title="correlogram with ggpairs()") 
 
 #β回归
-library(tidyverse)
-=======
+
+
 reg <- reg %>%
   mutate(growth_rate = log(DBH2)/log(DBH1),
          growth_rate = ifelse(is.infinite(growth_rate) | is.nan(growth_rate), 0, growth_rate))
@@ -143,6 +79,8 @@ load("data/data_for_reg.RData")
 #未标准化的数据
 #####
 library(ggplot2)
+library(tidyverse)
+library(dplyr)
 
 # 选择数值型变量列
 numeric_vars <- reg[, c("qr_AM", "qr_EM", "qr_BZ", "qr_Pn", "qr_Pq", "GX", "GY", "DBH1", "DBH2", "AD", "SRL", "SRA", "soc", "tn", "tp", "ap", "ph", "pd50_unweigh", "mpd50_unweigh", "mpd50_weigh", "mntd50_unweigh", "mntd50_weigh", "pd20_unweigh", "mpd20_unweigh", "mpd20_weigh", "mntd20_unweigh", "mntd20_weigh", "pd10_unweigh", "mpd10_unweigh", "mpd10_weigh", "mntd10_unweigh", "mntd10_weigh", "shannon_div_20", "invsimpson_div_20", "simpson_div_20", "shannon_div_10", "invsimpson_div_10", "simpson_div_10", "shannon_div_50", "invsimpson_div_50", "simpson_div_50", "BD_20", "CBD_20", "HBD_20", "BD_10", "CBD_10", "HBD_10", "BD_50", "CBD_50", "HBD_50", "growth_rate")]
@@ -185,7 +123,7 @@ high_corr_pairs <- data.frame(
 p <- ggplot(high_corr_pairs, aes(x = Variable1, y = Variable2, fill = Correlation)) +
   geom_tile() +
   geom_text(aes(label = ifelse(PValue < 0.05, "*", "")), color = "black", size = 6) +
-  scale_fill_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0.8) +
+  scale_fill_gradient2(low = "#e5f5f9", mid = "#99d8c9", high = "#2ca25f", midpoint = 0.8) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   labs(title = "Correlation Plot with Significance Annotations (origin)")
@@ -217,32 +155,30 @@ print(table_data)
 cor_matrix_kendall <- cor(numeric_vars, method = "kendall", use = "complete.obs")
 p_values_kendall <- matrix(NA, nrow = ncol(cor_matrix_kendall), ncol = ncol(cor_matrix_kendall))
 for (i in 1:(ncol(cor_matrix_kendall) - 1)) {
-for (j in (i + 1):ncol(cor_matrix_kendall)) {
-cor_test <- cor.test(numeric_vars[, i], numeric_vars[, j], method = "kendall")
-p_values_kendall[i, j] <- cor_test$p.value
-p_values_kendall[j, i] <- cor_test$p.value
+  for (j in (i + 1):ncol(cor_matrix_kendall)) {
+    cor_test <- cor.test(numeric_vars[, i], numeric_vars[, j], method = "kendall")
+    p_values_kendall[i, j] <- cor_test$p.value
+    p_values_kendall[j, i] <- cor_test$p.value
 }
 }
 # 将显著性矩阵命名为 p_values_kendall
 colnames(p_values_kendall) <- colnames(cor_matrix_kendall)
 rownames(p_values_kendall) <- colnames(cor_matrix_kendall)
-library(ggplot2)
-
 
 # 创建相关关系图数据框
 high_corr_pairs <- which(cor_matrix_kendall > 0.5 & cor_matrix_kendall < 1, arr.ind = TRUE)
 high_corr_vars <- rownames(cor_matrix_kendall)[high_corr_pairs[, 1]]
 high_corr_pairs <- data.frame(
-Variable1 = high_corr_vars[high_corr_pairs[, 1]],
-Variable2 = colnames(cor_matrix_kendall)[high_corr_pairs[, 2]],
-Correlation = cor_matrix_kendall[high_corr_pairs],
-PValue = p_values_kendall[high_corr_pairs]
-)
+  Variable1 = high_corr_vars[high_corr_pairs[, 1]],
+  Variable2 = colnames(cor_matrix_kendall)[high_corr_pairs[, 2]],
+  Correlation = cor_matrix_kendall[high_corr_pairs],
+  PValue = p_values_kendall[high_corr_pairs]
+  )
 # 创建相关关系图
 p <- ggplot(high_corr_pairs, aes(x = Variable1, y = Variable2, fill = Correlation)) +
 geom_tile() +
 geom_text(aes(label = ifelse(PValue < 0.05, "*", "")), color = "black", size = 6) +
-scale_fill_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0.8) +
+scale_fill_gradient2(low = "#e5f5f9", mid = "#99d8c9", high = "#2ca25f", midpoint = 0.8) +
 theme_minimal() +
 theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
 labs(title = "Correlation Plot with Significance Annotations (kendall)")
@@ -258,42 +194,42 @@ numeric_vars_zscore <- apply(numeric_vars, 2, function(x) (x - mean(x)) / sd(x))
 numeric_vars_zscore <- as.matrix(numeric_vars_zscore)
 
 # 计算相关性矩阵
-cor_matrix <- cor(numeric_vars_zscore, method = "kendall", use = "complete.obs")
+cor_matrix_zscore <- cor(numeric_vars_zscore, method = "kendall", use = "complete.obs")
 
 # 初始化显著性矩阵
-p_values <- matrix(NA, nrow = ncol(cor_matrix), ncol = ncol(cor_matrix))
+p_values_zscore <- matrix(NA, nrow = ncol(cor_matrix_zscore), ncol = ncol(cor_matrix_zscore))
 
 # 计算显著性矩阵
-for (i in 1:(ncol(cor_matrix) - 1)) {
-  for (j in (i + 1):ncol(cor_matrix)) {
+for (i in 1:(ncol(cor_matrix_zscore) - 1)) {
+  for (j in (i + 1):ncol(cor_matrix_zscore)) {
     cor_test <- cor.test(numeric_vars_zscore[, i], numeric_vars_zscore[, j], method = "kendall")
-    p_values[i, j] <- cor_test$p.value
-    p_values[j, i] <- cor_test$p.value
+    p_values_zscore[i, j] <- cor_test$p.value
+    p_values_zscore[j, i] <- cor_test$p.value
   }
 }
 
-# 将显著性矩阵命名为 p_values
-colnames(p_values) <- colnames(cor_matrix)
-rownames(p_values) <- colnames(cor_matrix)
+# 将显著性矩阵命名为 p_values_zscore
+colnames(p_values_zscore) <- colnames(cor_matrix_zscore)
+rownames(p_values_zscore) <- colnames(cor_matrix_zscore)
 
 library(ggplot2)
 
 # 创建相关关系图数据框
-high_corr_pairs <- which(cor_matrix > 0.5 & cor_matrix < 1, arr.ind = TRUE)
+high_corr_pairs <- which(cor_matrix_zscore > 0.5 & cor_matrix_zscore < 1, arr.ind = TRUE)
 
-high_corr_vars <- rownames(cor_matrix)[high_corr_pairs[, 1]]
+high_corr_vars <- rownames(cor_matrix_zscore)[high_corr_pairs[, 1]]
 high_corr_pairs <- data.frame(
   Variable1 = high_corr_vars[high_corr_pairs[, 1]],
-  Variable2 = colnames(cor_matrix)[high_corr_pairs[, 2]],
-  Correlation = cor_matrix[high_corr_pairs],
-  PValue = p_values[high_corr_pairs]
+  Variable2 = colnames(cor_matrix_zscore)[high_corr_pairs[, 2]],
+  Correlation = cor_matrix_zscore[high_corr_pairs],
+  PValue = p_values_zscore[high_corr_pairs]
 )
 
 # 创建相关关系图
 p <- ggplot(high_corr_pairs, aes(x = Variable1, y = Variable2, fill = Correlation)) +
   geom_tile() +
   geom_text(aes(label = ifelse(PValue < 0.05, "*", "")), color = "black", size = 6) +
-  scale_fill_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0.8) +
+  scale_fill_gradient2(low = "#e5f5f9", mid = "#99d8c9", high = "#2ca25f", midpoint = 0.8) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   labs(title = "Correlation Plot with Significance Annotations(zscore)")
@@ -320,7 +256,7 @@ print(table_data)
 #β回归
 library(tidyverse)
 #关于AM的部分
->>>>>>> 26443777ed9086be7130a97033f3852e9444c867
+
 beita_reg <- lm(qr_AM ~ shannon_div_20 + SRL + AD + mpd20_weigh + soc + tn + tp + ap + ph+ DBH2, data = reg)
 step_beita_reg <- step(beita_reg)
 summary(beita_reg)
